3c8da2ca8b91283c8a44afe32914f3443d34af8e
Anton Betsun <69806148+beetzung@users.noreply.github.com>
2022-11-19 13:36:21 +0100

    Add basic logic for main fragment

--- app/src/main/java/com/beetzung/helpie/ui/main/MainFragment.kt
+++ app/src/main/java/com/beetzung/helpie/ui/main/MainFragment.kt
@@ -1,20 +1,34 @@
 package com.beetzung.helpie.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.beetzung.helpie.R
+import com.beetzung.helpie.data.model.DaysData
 import com.beetzung.helpie.databinding.FragmentMainBinding
 import com.beetzung.helpie.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainFragment : BaseFragment(R.layout.fragment_main) {
     private val binding: FragmentMainBinding by viewBinding(FragmentMainBinding::bind)
     private val viewModel: MainViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.daysRecords.observe(viewLifecycleOwner) { data ->
+            binding.updateView(data)
+        }
+    }
 
+    private fun FragmentMainBinding.updateView(daysData: DaysData) {
+        val builder = StringBuilder()
+        for (key in daysData.keys) {
+            daysData[key]?.let {
+                builder.append("$key: $it\n")
+            }
+        }
+        mainText.text = builder.toString()
+    }
 }
--- app/src/main/java/com/beetzung/helpie/ui/main/MainViewModel.kt
+++ app/src/main/java/com/beetzung/helpie/ui/main/MainViewModel.kt
@@ -1,7 +1,19 @@
 package com.beetzung.helpie.ui.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.beetzung.helpie.data.Database
+import com.beetzung.helpie.data.model.DaysData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val database: Database
+): ViewModel() {
+    val daysRecords: LiveData<DaysData> = liveData {
+        database.getDaysData()
+    }
+
 }
