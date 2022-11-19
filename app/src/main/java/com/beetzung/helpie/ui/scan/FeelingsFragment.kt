826e551a71776d6bd1adbe8baf4766a27847c87f
Anton Betsun <69806148+beetzung@users.noreply.github.com>
2022-11-19 19:16:57 +0100

    Add UI

--- app/src/main/java/com/beetzung/helpie/ui/scan/FeelingsFragment.kt
+++ app/src/main/java/com/beetzung/helpie/ui/scan/FeelingsFragment.kt
@@ -5,10 +5,11 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.beetzung.helpie.R
 import com.beetzung.helpie.databinding.FragmentFeelingsBinding
 import com.beetzung.helpie.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FeelingsFragment : BaseFragment(R.layout.fragment_feelings) {
     private val binding: FragmentFeelingsBinding by viewBinding(FragmentFeelingsBinding::bind)
     private val viewModel: FeelingsViewModel by viewModels()
-
 }
 
