3d14e15b8618b38b2e20cc2d9aa2323fa99ece48
Anton Betsun <69806148+beetzung@users.noreply.github.com>
2022-11-20 01:42:46 +0100

    Add blank fragments

---
+++ app/src/main/java/com/beetzung/helpie/ui/main/FavoritesFragment.kt
@@ -0,0 +1,6 @@
+package com.beetzung.helpie.ui.main
+
+import com.beetzung.helpie.R
+import com.beetzung.helpie.ui.BaseFragment
+
+class FavoritesFragment : BaseFragment(R.layout.fragment_favorites)
---
+++ app/src/main/java/com/beetzung/helpie/ui/main/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.beetzung.helpie.ui.main
+
+import com.beetzung.helpie.R
+import com.beetzung.helpie.ui.BaseFragment
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings)
